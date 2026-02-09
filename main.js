b310ef2a466c035376dfd561a99456208371ac0d
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-09 16:51:03 +0100

    implement getDashboardData with parallel API calls using Promise.all
    - Fetched city and country from /destinations
    - Fetched current weather from /weathers
    - Fetched main airport from /airports
    - Aggregated results into a single object with properties: city, country, temperature, weather, airport
    - Calls to external APIs are executed in parallel for improved performance
    - Added example usage with formatted console output

---
+++ main.js
@@ -0,0 +1,46 @@
+/*In questo esercizio, utilizzerai Promise.all() per creare la funzione getDashboardData(query), 
+che accetta una città come input e recupera simultaneamente:
+Nome completo della città e paese da  /destinations?search=[query]
+(result.name, result.country, nelle nuove proprietà city e country).
+Il meteo attuale da /weathers?search={query}
+(result.temperature e result.weather_description nella nuove proprietà temperature e weather).
+Il nome dell’aeroporto principale da /airports?search={query}
+(result.name nella nuova proprietà airport).
+Utilizzerai Promise.all() per eseguire queste richieste
+in parallelo e poi restituirai un oggetto con i dati aggregati.*/
+
+async function fetchJason(url) {
+    const response = await fetch(url)
+    const obj = await response.json()
+    return obj
+}
+
+async function getDashboardData(query) {
+    const [getDestination, getWeather, getAirport] = await Promise.all([
+        fetchJason(`http://localhost:3333/destinations?search=${query}`),
+        fetchJason(`http://localhost:3333/weathers?search=${query}`),
+        fetchJason(`http://localhost:3333/airports?search=${query}`)
+    ])
+    const destination = getDestination[0];
+    const weather = getWeather[0];
+    const airport = getAirport[0];
+
+    const data = {
+        city: destination.name,
+        country: destination.country,
+        temperature: weather.temperature,
+        weather: weather.weather_description,
+        airport: airport.name
+    };
+    return data
+
+}
+
+(async () => {
+    const data = await getDashboardData('London')
+    console.log('Dashboard data', data)
+    console.log(`${data.city} is in ${data.country}.\n` +
+        `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+        `The main airport is ${data.airport}.\n`)
+})()
+
