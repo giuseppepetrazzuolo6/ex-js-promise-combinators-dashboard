d98fbb971de2e21c1f5bb2cd146183c977199023
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-10 09:29:34 +0100

    fix output console

--- main.js
+++ main.js
@@ -44,15 +44,17 @@ async function getDashboardData(query) {
     try {
         const data = await getDashboardData('Vienna')
         console.log('Dashboard data', data)
-        if (data.city && data.country) {
-            console.log(`${data.city} is in ${data.country}.`)
+        let frase = ''
+        if (data.city !== null && data.country !== null) {
+            frase += `${data.city} is in ${data.country}.\n`
         }
-        if (data.temperature !== null && data.weather) {
-            console.log(`Today there are ${data.temperature} degrees and the weather is ${data.weather}`)
+        if (data.temperature !== null && data.weather !== null) {
+            frase += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`
         }
-        if (data.airport) {
-            console.log(`The main airport is ${data.airport}`)
+        if (data.airport !== null) {
+            frase += `The main airport is ${data.airport}.\n`
         }
+        console.log(frase)
     } catch (error) {
         console.error('Errore dashboard:', error.message);
     }
